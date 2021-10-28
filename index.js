60e85b275e70df885e1785368b9331483fd4a09f
DanielPhilosoph <danielph9@gmail.com>
2021-10-28 13:35:59 +0300

    basic idea of how the code would go

---
+++ index.js
@@ -0,0 +1,27 @@
+class view {
+  constructor() {
+    // Create board
+  }
+
+  // functions:
+  // 1. place a coin - make the turn
+  // 2. winner
+  // 3. change players turns (change a label)
+}
+class module {
+  constructor(player1, player2, color1, color2) {
+    // Create data strucure
+    this._turn;
+    this._winner;
+    this._players = [player1, player2];
+    this._colors = [color1, color2];
+    this._board = [];
+  }
+
+  //functions
+  // 1. change turn
+  // 2. playing turn (adding to board)
+  // 3. winner check (check if there is a 4 in a row)
+  // 4. reset game
+}
+class controller {}
